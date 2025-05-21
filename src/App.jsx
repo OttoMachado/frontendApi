c4d07be0f330cfd6624396072ea2a05de8eceb63
OttoMachado <158228292+OttoMachado@users.noreply.github.com>
2025-05-21 20:51:16 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "https://backendloja-ex2s.onrender.com"; // substituir pela URL da api do render AULA 22/05
+const API_URL = "https://backendapi-7z6f.onrender.com"; // substituir pela URL da api do render AULA 22/05
 
 export default function App() {
   const [produtos, setProdutos] = useState([]);
